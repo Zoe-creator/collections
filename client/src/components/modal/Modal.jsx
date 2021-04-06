75fb36d4d2cd4dac721caa6155228c86db3cd4f7
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-04-05 21:55:01 -0400

    done with modal css

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -6,7 +6,7 @@ export default function Modal(props) {
   return (
     <div className='modal-container' onClick={(e) => handleOpen(false)}>
       <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <button classname="cancel-delete" onClick={() => handleOpen(false)}>❎</button>
+        <button id="cancel-delete" onClick={() => handleOpen(false)}>X</button>
         <div className="delete-confirmed">
           <p className="delete-confirm-message">Are you sure?</p>
           <button onClick={() => {
