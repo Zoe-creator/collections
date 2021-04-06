377f775d41e98c77dbd16a90210a6b0d1b8dcce7
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-04-05 20:51:27 -0400

    emoji cancel delete

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -5,8 +5,8 @@ export default function Modal(props) {
   return (
     <div className='modal-container' onClick={(e) => handleOpen(false)}>
       <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <p>Are you sure?</p>
-        <button onClick={() => handleOpen(false)}>no</button>
+        <p className="delete-confirm-message">Are you sure?</p>
+        <button classname="cancel-delete"onClick={() => handleOpen(false)}>❎</button>
         <button onClick={() => {
           handleDelete(open)
           handleOpen(false)
