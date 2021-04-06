064ccae6019a67f499c06bf3bf9633e6ced41b59
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-04-05 21:20:22 -0400

    modal

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -5,12 +5,15 @@ export default function Modal(props) {
   return (
     <div className='modal-container' onClick={(e) => handleOpen(false)}>
       <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <p className="delete-confirm-message">Are you sure?</p>
-        <button classname="cancel-delete"onClick={() => handleOpen(false)}>❎</button>
-        <button onClick={() => {
-          handleDelete(open)
-          handleOpen(false)
-        }}>✅</button>
+        <button classname="cancel-delete" onClick={() => handleOpen(false)}>❎</button>
+        <div className="delete-confirmed">
+          <p className="delete-confirm-message">Are you sure?</p>
+          <button onClick={() => {
+            handleDelete(open)
+            handleOpen(false)
+          }}>✅</button>
+        </div>
+
       </div>
     </div>
   )
